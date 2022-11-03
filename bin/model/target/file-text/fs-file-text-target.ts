5829d2e895675b92be0a61215deb95b28a31ee34
Ace Code <ace.code.pt@gmail.com>
2022-11-03 20:06:40 +0200

    added support for recurcieve

--- bin/model/target/file-text/fs-file-text-target.ts
+++ bin/model/target/file-text/fs-file-text-target.ts
@@ -1,10 +1,19 @@
 import fs from "fs/promises";
+import path from "path";
 import { FileTextNotifyExceptions } from "./exceptions";
 import { IFileTextTarget } from "./file-text-target.interace";
 import { FileTextTargetParameters } from "./types";
 
 export class FsFileTextTarget implements IFileTextTarget {
 	async notify({ filePath, fileContent }: FileTextTargetParameters): Promise<void> {
+		// Get the dir of a the file
+		const passedPath = path.dirname(filePath);
+		// make the directory, recursively.
+		await fs.mkdir(passedPath, { recursive: true }).catch((e) => {
+			throw new FileTextNotifyExceptions(
+				`Failed to create the directories of path ${passedPath} recursivley because of: ${e}`,
+			);
+		});
 		await fs.writeFile(filePath, fileContent).catch((e) => {
 			throw new FileTextNotifyExceptions(`Failed to create the file ${filePath} using FS because of: ${e}`);
 		});
