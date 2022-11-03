1d1e7e72bd9b5f08d0d7d19187d2f72048fcb54f
Ace Code <ace.code.pt@gmail.com>
2022-11-03 19:50:54 +0200

    changed the meaning of a target a bit

--- bin/model/target/target.interface.ts
+++ bin/model/target/target.interface.ts
@@ -1,3 +1,10 @@
-export interface ITarget<T, P> {
-	notify(data: P): T;
+// Typescript currently unable to condition the never type directly so there is a work around
+export type ITarget<T, P = never> = [P] extends [never] ? ITargetWithoutParams<T> : ITargetWithParams<T, P>;
+
+interface ITargetWithoutParams<T> {
+	notify(): T;
+}
+
+interface ITargetWithParams<T, P> {
+	notify(payload: P): T;
 }
