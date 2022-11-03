84b6f9dc6711c75c4faf755089bac9239267deaa
Ace Code <ace.code.pt@gmail.com>
2022-11-03 19:52:28 +0200

    added file text

--- bin/model/source/index.ts
+++ bin/model/source/index.ts
@@ -1,5 +1,6 @@
 export * from "./exceptions";
 export * from "./file-path-collection-of-folder";
+export * from "./file-text";
 export * from "./folder-collection-of-folder";
 export * from "./model-folders";
 export * from "./source.interface";
