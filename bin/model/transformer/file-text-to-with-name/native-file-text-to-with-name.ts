c964537130f551eb8b6b541c11df2dfb93c573fb
Ace Code <ace.code.pt@gmail.com>
2022-11-04 07:43:13 +0200

    did a proper name replace

--- bin/model/transformer/file-text-to-with-name/native-file-text-to-with-name.ts
+++ bin/model/transformer/file-text-to-with-name/native-file-text-to-with-name.ts
@@ -1,8 +1,12 @@
+import kebabCase from "lodash.kebabcase";
+import upperFirst from "lodash.upperfirst";
 import { IFileTextToWithNameTransformer } from "./file-text-to-with-name.interface";
 import { FileTextReplacedName, FileTextToWithNameParametrs } from "./types";
 
 export class NativeFileTextToWithName implements IFileTextToWithNameTransformer {
 	transform({ fileText, findText, replaceTextWith }: FileTextToWithNameParametrs): FileTextReplacedName {
-		return fileText.replaceAll(findText, replaceTextWith);
+		return fileText
+			.replaceAll(findText, replaceTextWith)
+			.replaceAll(upperFirst(kebabCase(findText)), upperFirst(kebabCase(replaceTextWith)));
 	}
 }
