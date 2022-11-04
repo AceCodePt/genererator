6c597fa76cd94dafeb097fdff0c348c21e17ad76
Ace Code <ace.code.pt@gmail.com>
2022-11-04 07:57:32 +0200

    done with part one of this side project

--- bin/main.ts
+++ bin/main.ts
@@ -7,12 +7,18 @@ import { InquirerModelKindPath, InquirerModelKindTypePathInput, InquirerModelNam
 import path from "path";
 import { argv } from "process";
 import { NativePathToNameValue } from "./model/transformer";
-import { GlobFilePathCollectionOfFolderSource, GlobFolderCollectionOfFolderSource } from "./model/source";
+import {
+	FsFileTextSource,
+	GlobFilePathCollectionOfFolderSource,
+	GlobFolderCollectionOfFolderSource,
+} from "./model/source";
 import { FsFileTextTarget } from "./model/target";
+import { NativeFileTextToWithName } from "./model/transformer";
 
 // We say that the first argv is the current working dir unless specified otherwise
-const currDir = path.resolve(process.cwd(), argv[2] ?? "./");
+const projectDir = path.resolve(process.cwd(), argv[2] ?? "./");
 const scriptPath = path.resolve(argv[1], "../../");
+const assetPath = path.resolve(scriptPath, "./assets");
 clear();
 console.log(chalk.green(figlet.textSync("generator", { horizontalLayout: "full" })));
 
@@ -20,8 +26,9 @@ async function main() {
 	const folderCollectionOfFolder = new GlobFolderCollectionOfFolderSource(glob);
 	const filePathCollectionOfFolder = new GlobFilePathCollectionOfFolderSource(glob);
 	const pathToNameValue = new NativePathToNameValue();
-	const assetPath = path.resolve(scriptPath, "./assets");
+	const fileTextSource = new FsFileTextSource();
 	const fileTextTarget = new FsFileTextTarget();
+	const fileTextToNameTransformer = new NativeFileTextToWithName();
 
 	// Get the model types folders
 	const modelTypesFolders = await folderCollectionOfFolder.ask({ dir: assetPath });
@@ -31,9 +38,68 @@ async function main() {
 	const selectedName = await new InquirerModelName().run();
 
 	// Get from the model kind path the file in the path
-	const modelKindTypesFiles = await filePathCollectionOfFolder.ask({
-		dir: path.resolve(modelKindPath, "./name"),
-	});
+	const [modelKindTypesFiles, modelKindFiles] = await Promise.all([
+		filePathCollectionOfFolder.ask({
+			dir: path.resolve(modelKindPath, "./name"),
+		}),
+		filePathCollectionOfFolder.ask({
+			dir: modelKindPath,
+		}),
+	]);
 	const modelKindTypePath = await new InquirerModelKindTypePathInput(pathToNameValue).run(modelKindTypesFiles);
+	const projectModelPath = await glob("**/model/", { cwd: projectDir, absolute: true }).then(([folder]) =>
+		path.normalize(folder),
+	);
+
+	// Remove all the files that contain -name
+	const modelKindTypeFilesToCreate = modelKindTypesFiles.filter((path) => !path.includes("-name"));
+
+	// Add the selected file
+	modelKindTypeFilesToCreate.push(modelKindTypePath);
+
+	// Run through each file and create it where it belongs
+	await Promise.all(
+		modelKindTypeFilesToCreate.map(async (assetFilePath) => {
+			const fileContentNonCompiled = await fileTextSource.ask({ dir: assetFilePath });
+			let fileContent = fileTextToNameTransformer.transform({
+				fileText: fileContentNonCompiled,
+				findText: "name",
+				replaceTextWith: selectedName,
+			});
+
+			// /target/name/check
+			const relativePathToFile = path
+				.normalize(assetFilePath)
+				.replace(path.normalize(assetPath), "")
+				.replaceAll("name", `${selectedName}`);
+
+			// proj-ex/src/model/target/exceptions.ts
+			const projectFilePath = path.join(projectModelPath, relativePathToFile);
+
+			if (projectFilePath.includes("index.ts")) {
+				fileContent = `${fileContent}\nexport * from "./${modelKindTypePath
+					.split("/")
+					.slice(-1)[0]
+					.replace("name", selectedName)}"`.replace(".ts", "");
+			}
+
+			await fileTextTarget.notify({ filePath: projectFilePath, fileContent });
+		}),
+	);
+
+	// Run through all the kind files and create them if they don't exist
+	await Promise.all(
+		modelKindFiles.map(async (assetFilePath) => {
+			let fileContent = await fileTextSource.ask({ dir: assetFilePath });
+
+			// /target/exceptions.ts
+			const relativePathToFile = path.normalize(assetFilePath).replace(path.normalize(assetPath), "");
+
+			// proj-ex/src/model/target/exceptions.ts
+			const projectFilePath = path.join(projectModelPath, relativePathToFile);
+
+			await fileTextTarget.notify({ filePath: projectFilePath, fileContent });
+		}),
+	);
 }
 main();
